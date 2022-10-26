1c675d078fe653c3d989cbddf684a80b54182bbc
Igor Sprovieri <igorsprovieri3@gmail.com>
2022-10-26 16:29:36 -0300

    First

---
+++ Assets/Foods/Food.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Food", menuName = "Food")]
+public class Food : ScriptableObject
+
+{
+    public string Name;
+    public Sprite Image;
+    public int UnlockLevel;
+    public float PreparationTime;
+    public int QuantityCooked;
+    public GameObject Prefab;
+    public int Cost, Price;
+}
